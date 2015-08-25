8beb74ebfc7b2a16f82c4b9f3dec6b29b1cd780d
Luis Menina <liberforce@freeside.fr>
2015-08-25 23:10:20 +0200

    Remove formats and fix output filename

--- meizu-gst-convert.sh
+++ meizu-gst-convert.sh
@@ -8,14 +8,14 @@ height=240
 gst-launch-1.0 filesrc location="$filename" \
 	! decodebin name=decoder decoder.! queue \
 	! videorate ! videoscale \
-	! "video/x-raw,format=yuv,framerate=(fraction)${fps}/1,width=${width},height=${height}" \
+	! "video/x-raw,framerate=(fraction)${fps}/1,width=${width},height=${height}" \
 	! videoflip method=clockwise \
 	! avenc_mpeg4 bitrate=384000 max-bframes=0 \
 	! queue \
 	! avimux name=mux \
 	! progressreport \
-	! filesink location="gst-$filename" decoder.! queue \
+	! filesink location="$filename-gst.avi" decoder.! queue \
 	! audioconvert \
-	! audioresample ! "audio/x-raw,format=int,channels=2,rate=44100" \
+	! audioresample ! "audio/x-raw,channels=2,rate=44100" \
 	! lamemp3enc cbr=true bitrate=128 \
 	! mux.
