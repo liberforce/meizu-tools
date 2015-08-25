fd206710773c33e822b5d4a8038d6f4f62155888
Luis Menina <liberforce@freeside.fr>
2015-08-25 22:23:36 +0200

    Spread on multiple lines for diff's

--- meizu-gst-convert.sh
+++ meizu-gst-convert.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
 filename="$1"
 
-gst-launch-1.0 filesrc location="$filename" ! decodebin name=decoder decoder.! queue ! videorate ! videoscale ! 'video/x-raw-yuv,framerate=(fraction)20/1,width=320,height=240' ! videoflip method=clockwise ! avenc_mpeg4 bitrate=384000 max-bframes=0 ! queue ! avimux name=mux ! progressreport ! filesink location="gst-$filename" decoder.! queue ! audioconvert ! audioresample ! 'audio/x-raw-int,channels=2,rate=44100' ! lamemp3enc mode=stereo vbr=none bitrate=128 ! mux.
+gst-launch-1.0 filesrc location="$filename" \
+	! decodebin name=decoder decoder.! queue \
+	! videorate ! videoscale \
+	! 'video/x-raw-yuv,framerate=(fraction)20/1,width=320,height=240' \
+	! videoflip method=clockwise \
+	! avenc_mpeg4 bitrate=384000 max-bframes=0 \
+	! queue \
+	! avimux name=mux \
+	! progressreport \
+	! filesink location="gst-$filename" decoder.! queue \
+	! audioconvert \
+	! audioresample ! 'audio/x-raw-int,channels=2,rate=44100' \
+	! lamemp3enc mode=stereo vbr=none bitrate=128 \
+	! mux.
