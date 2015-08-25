498b9408f68a54ba09c1dd269f3f4b62731e5e9e
Luis Menina <liberforce@freeside.fr>
2015-08-25 22:38:04 +0200

    Use gstreamer 1.0 raw caps format

--- meizu-gst-convert.sh
+++ meizu-gst-convert.sh
@@ -8,7 +8,7 @@ height=240
 gst-launch-1.0 filesrc location="$filename" \
 	! decodebin name=decoder decoder.! queue \
 	! videorate ! videoscale \
-	! "video/x-raw-yuv,framerate=(fraction)${fps}/1,width=${width},height=${height}" \
+	! "video/x-raw,format=yuv,framerate=(fraction)${fps}/1,width=${width},height=${height}" \
 	! videoflip method=clockwise \
 	! avenc_mpeg4 bitrate=384000 max-bframes=0 \
 	! queue \
@@ -16,6 +16,6 @@ gst-launch-1.0 filesrc location="$filename" \
 	! progressreport \
 	! filesink location="gst-$filename" decoder.! queue \
 	! audioconvert \
-	! audioresample ! 'audio/x-raw-int,channels=2,rate=44100' \
+	! audioresample ! "audio/x-raw,format=int,channels=2,rate=44100" \
 	! lamemp3enc cbr=true bitrate=128 \
 	! mux.
