7cb281aab03a742c4a1bde36cabee751a797e14a
Luis Menina <liberforce@freeside.fr>
2015-08-25 22:27:14 +0200

    Extract constants

--- meizu-gst-convert.sh
+++ meizu-gst-convert.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 filename="$1"
 
+fps=20
+width=320
+height=240
+
 gst-launch-1.0 filesrc location="$filename" \
 	! decodebin name=decoder decoder.! queue \
 	! videorate ! videoscale \
-	! 'video/x-raw-yuv,framerate=(fraction)20/1,width=320,height=240' \
+	! "video/x-raw-yuv,framerate=(fraction)${fps}/1,width=${width},height=${height}" \
 	! videoflip method=clockwise \
 	! avenc_mpeg4 bitrate=384000 max-bframes=0 \
 	! queue \
