d0aa2eecaa39a6be30861c9af7cd76b2829438b9
Luis Menina <liberforce@freeside.fr>
2015-09-11 01:07:02 +0200

    Split over multiple lines to ease debugging

--- meizu-mconvert.sh
+++ meizu-mconvert.sh
@@ -2,5 +2,17 @@
 
 for f in "$@"
 do
-mencoder "$f" -idx -noodml -ofps 20 -vf scale=320:-2,expand=:240:::1,crop=320:240,rotate=1 -ovc lavc -ffourcc XVID -lavcopts vcodec=mpeg4:vbitrate=384:vmax_b_frames=0:vhq -sws 9 -srate 44100 -oac mp3lame -lameopts cbr:br=128:mode=0 -o "meizu-${f%.*}.avi"
+	mencoder "$f"                                           \
+	-idx                                                    \
+	-noodml                                                 \
+	-ofps 20                                                \
+	-vf scale=320:-2,expand=:240:::1,crop=320:240,rotate=1  \
+	-ovc lavc                                               \
+	-ffourcc XVID                                           \
+	-lavcopts vcodec=mpeg4:vbitrate=384:vmax_b_frames=0:vhq \
+	-sws 9                                                  \
+	-srate 44100                                            \
+	-oac mp3lame                                            \
+	-lameopts cbr:br=128:mode=0                             \
+	-o "meizu-${f%.*}.avi"
 done 
