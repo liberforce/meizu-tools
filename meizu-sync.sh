ab687e359459ac546856224522468e3f7f23ddf8
Luis Menina <liberforce@freeside.fr>
2015-09-11 02:32:25 +0200

    Clean set of rsync options

--- meizu-sync.sh
+++ meizu-sync.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# l'option -n fait un dry run
-#rsync -rLtvvu ~/Musique/Meizu/ /media/68DE-5DE7/
-# good one: rsync -rLtvv --modify-window=2 --delete --progress -h ~/Musique/Meizu/ /media/68DE-5DE7/
-#rsync -rLtv --size-only --delete --progress -h ~/Musique/Meizu/ /media/68DE-5DE7/
 
-rsync -rLtvv --modify-window=2 --delete --progress -h /run/media/luis/DATA/musique/Meizu/ /run/media/luis/68DE-5DE7/
+# Use -n to perform a dry run
+rsync -a --copy-links --delete --modify-window=2 --progress /run/media/luis/DATA/musique/Meizu/ /run/media/luis/68DE-5DE7/
