6e84aaae53bd18b9a9210378ec70d535d3918d66
Luis Menina <liberforce@freeside.fr>
2015-09-11 02:55:13 +0200

    Change window to the one usually used for FAT filesystems

--- meizu-sync.sh
+++ meizu-sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Use -n to perform a dry run
-rsync -a --copy-links --delete --modify-window=2 --progress /run/media/luis/DATA/musique/Meizu/ /run/media/luis/68DE-5DE7/
+rsync -a --copy-links --delete --modify-window=1 --progress /run/media/luis/DATA/musique/Meizu/ /run/media/luis/68DE-5DE7/
